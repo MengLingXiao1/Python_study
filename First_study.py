e60b01b19b00efdc8a6e5fc41318cca135072bd5
MengLingXiao1 <854935194@qq.com>
2017-08-10 23:47:38 +0800

    更新
    捕捉异常

--- First_study.py
+++ First_study.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 # @Author  : Mlx
 from selenium import webdriver
+import time
 driver=webdriver.Chrome()
-driver.get("https://www.baidu.com")
-print driver.name
-print driver.title
+try:
+    driver.get("https://www.baidu.com")
+except:
+    print driver.name
+    print driver.title
+finally:
+    print time.ctime()
